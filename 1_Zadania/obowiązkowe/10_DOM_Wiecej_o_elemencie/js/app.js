5e4247dda44dd227c3789df0a1bda557a5de75cf
MrKubaD <jakub.dedio@microsoft.wsei.edu.pl>
2019-12-14 14:59:22 +0100

    chapter 10 task 0

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -2,5 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
-
+    // Zadanie 0
+    let task0ListElement = document.querySelectorAll('.ex5 li');
+    task0ListElement[4].classList.add('big');
+    for(let i = 0; i < task0ListElement.length; ++i) {
+        if(i % 2 == 0)
+            task0ListElement[i].style.backgroundColor = 'green';
+        if(i % 3 == 0)
+             task0ListElement[i].style.textDecoration = 'underline';
+    }
 });
