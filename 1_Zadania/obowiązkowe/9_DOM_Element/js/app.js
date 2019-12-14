b52ef8b8d363337657a65cf15803dfb62b4e4339
MrKubaD <jakub.dedio@microsoft.wsei.edu.pl>
2019-12-14 14:42:55 +0100

    chapter 9 tasks

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,85 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    //Zadanie 0
+    const getDatasInfo = (elements) => {
+        const arr = [];
+
+        for(let i = 0; i < elements.length; i++){
+            arr.push(elements[i].dataset.color);
+        }
+
+        return arr;
+    }
+
+    console.log(getDatasInfo(links));
+
+    //Zadanie 1
+    console.log(homeElement);
+    console.log(childElements);
+    console.log(banner);
+    console.log(blocks);
+    console.log(links);
+
+    for(let i = 0; i < childElements.length; i++){
+        console.log('tag: ' + childElements[i].tagName, 'klasa: ' + childElements[i].className);
+    }
+
+    for(let i = 0; i < blocks.length; i++){
+        console.log('tag: ' + blocks[i].tagName, 'klasa: ' + blocks[i].className);
+    }
+
+    for(let i = 0; i < links.length; i++){
+        console.log('tag: ' + links[i].tagName, 'klasa: ' + links[i].className);
+    }
+
+    //Zadanie 2
+    for(let i = 0; i < blocks.length; i++){
+        //Wyświetla wewnętrzny kod HTML elementu
+        console.log(blocks[i].innerHTML);
+        //Wyświetla cały kod HTML elementu
+        console.log(blocks[i].outerHTML);
+        //Zmienia wewnętrzny kod HTML elementu
+        blocks[i].innerHTML = 'Nowa wartość diva o klasie blocks';
+    }
+
+    //Zadanie 3
+    const mainFooter = document.querySelector('#mainFooter');
+    const getId = (element) => {
+        console.log('id: ' + element.id);
+    }
+
+    console.log(getId(mainFooter));
+
+    //Zadanie 4
+    const getTags = (elements) => {
+        const arr = [];
+
+        for(let i = 0; i < elements.length; i++){
+            arr.push(elements[i].tagName);
+        }
+
+        return arr;
+    }
+
+    console.log(getTags(childElements));
+
+    //Zadanie 5
+    const getClassInfo = (element) => {
+        return element.className.split(' ');
+    }
+    console.log(getClassInfo(banner));
+
+    // Zadanie 6
+
+    let everyLielementInsideNavTag = document.querySelectorAll('nav li');
+    console.log(everyLielementInsideNavTag);
+
+    const setDataDirection = elements => elements.forEach(elem => {
+        if(!elem.hasAttribute('data-direction'))
+            elem.setAttribute('data-direction', 'top');
+
+    });
+    setDataDirection(everyLielementInsideNavTag);
+
 });
