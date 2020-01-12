33261c6fa8857122cbec00f44f8ad589b56f51b0
MrKubaD <jakub.dedio@microsoft.wsei.edu.pl>
2020-01-12 23:14:38 +0100

    additional task 11 solved

--- 1_Zadania/dodatkowe/11_Array_operations/js/app.js
+++ 1_Zadania/dodatkowe/11_Array_operations/js/app.js
@@ -1,7 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Zadanie 0
+    let cities = ["Kraków", "Olsztyn", "Szczecin", "Ostrów Wielkopolski"];
+    let citiesResult = cities.map(e => e.length);
+    console.log(citiesResult);
+    // Zadanie 1
+    function randomize(param1, param2, callback) {
+        if(param2 < param1) {
+            let t = param1;
+            param1 = param2;
+            param2 = t;
+        }
+        let result = Math.floor(Math.random() * (param2 - param1)) + param1;
+        if(typeof callback === 'function') {
+            callback(result);
+        }
+    }
+
+    randomize(2, 22, console.log);
+    // Zadanie 2
+    let animals = ['cat', 'shrimp', 'giraffe'];
+    animals.forEach(e => {
+        console.log(e);
+    });
+
+    // Zadanie 3
+    let years = [1995, 1856, 2014, 1987];
+    let resultYears = years.map(e => new Date().getFullYear() - e);
+    console.table(resultYears);
+
+    // Zadanie 4
     var numbers = [5, 10, 12, 20, 100];
+    // 4.1
+    var sumOfNumbers = numbers.reduce((prev, curr, index, array) => prev + curr);
+    console.log(sumOfNumbers);
+    // 4.2
+        
     var result = numbers.reduce(function (prev, curr, index, array) {
         return prev * curr;
-    })
+    });
     console.log(result);
+
+
+
+
+
+
 })
