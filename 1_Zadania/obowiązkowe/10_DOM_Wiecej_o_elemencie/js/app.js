8049c7612a303c19139b96c0935a8dd093c7e213
MrKubaD <jakub.dedio@microsoft.wsei.edu.pl>
2020-01-13 13:58:27 +0100

    mandatory task 10 solved

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -11,4 +11,46 @@ document.addEventListener("DOMContentLoaded", function () {
         if(i % 3 == 0)
              task0ListElement[i].style.textDecoration = 'underline';
     }
+    // Zadanie 1
+    const chromeElement = document.querySelector('.chrome');
+    const firefoxElement = document.querySelector('.firefox');
+    const edgeElement = document.querySelector('.edge');
+
+    chromeElement.style.backgroundImage = 'url(./assets/img/chrome.png)';
+    edgeElement.style.backgroundImage = 'url(./assets/img/edge.png)';
+    firefoxElement.style.backgroundImage = 'url(./assets/img/firefox.png)';
+
+    chromeElement.nextElementSibling.href = 'https://www.google.com';
+    chromeElement.nextElementSibling.innerHTML = 'Google Chrome';
+    edgeElement.nextElementSibling.href = 'https://www.microsoft.com';
+    edgeElement.nextElementSibling.innerHTML = 'Microsoft Edge';
+    firefoxElement.nextElementSibling.href = 'https://www.mozilla.com';
+    firefoxElement.nextElementSibling.innerHTML = 'Mozilla Firefox';
+
+    chromeElement.style.width = '100px';
+
+    // Zadnie 2
+    const spanElements = document.querySelector('.ex2').querySelectorAll('span');
+    spanElements[0].innerHTML = 'Jakub Dedio';
+    spanElements[1].innerHTML = 'niebieski';
+    spanElements[2].innerHTML = 'nie mam żadnej';
+
+
+    // Zadanie 3
+    const listElements = document.querySelector('.ex3').querySelectorAll('li');
+    document.querySelector('.ex3').querySelector('ul').classList.add('menu');
+    for(let listElement of listElements) {
+        listElement.classList.add('menuElement');
+        listElement.classList.remove('error');
+    }
+
+    // Zadnie 4
+    const listElementsEx4 = document.querySelector('.ex4').querySelectorAll('li');
+    let counterEx4 = 1;
+    for(let listElementEx4 of listElementsEx4) {
+        listElementEx4.dataset.id = counterEx4;
+        counterEx4++;
+    }
+
+
 });
